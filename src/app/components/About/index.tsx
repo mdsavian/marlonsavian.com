d634b3dfe18405b4a76474f836b477dfd9a95fc5
mdsavian <marlon.savian@gmail.com>
2024-01-15 16:35:57 -0300

    feat: add materialUI emotion and clean up project

---
+++ src/app/components/About/index.tsx
@@ -0,0 +1,15 @@
+const About: React.FC = () => {
+  return (
+    <section
+      style={{
+        background: "linear-gradient(90deg, #4831d4 67%, #ccf381 33%)",
+        height: "100px",
+        width: "100%",
+      }}
+    >
+      <div>Ola</div>
+    </section>
+  );
+};
+
+export default About;

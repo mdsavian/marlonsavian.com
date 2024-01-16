d7854b2dc168d755b5835d7dd0f91933a70fd446
mdsavian <marlon.savian@gmail.com>
2024-01-16 20:47:59 -0300

    fix: header menu icon color

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -8,35 +8,43 @@ import { IconButton, Link } from "@mui/material";
 
 const Header = () => {
   return (
-    <AppBar
-      position="fixed"
-      sx={{
-        zIndex: 2000,
-        background: "transparent",
-        boxShadow: "none",
-        paddingLeft: {
-          md: "6rem",
-          xs: "3rem",
-        },
-        paddingRight: {
-          md: "6rem",
-          xs: "3rem",
-        },
-        height: "8em",
-        justifyContent: "center",
-      }}
-    >
-      <Toolbar>
-        <Link href="/" color="secondary" sx={{ textDecoration: "none", flexGrow: 1 }}>
-          <Typography variant="h4" color="secondary" fontWeight={600}>
-            Marlon Savian
-          </Typography>
-        </Link>
-        <IconButton aria-label="menu">
-          <MenuIcon fontSize="large" color="primary" sx={{ width: "1.2em", height: "1.2em" }} />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ display: "flex" }}>
+      <AppBar
+        sx={{
+          zIndex: 2000,
+          background: "transparent",
+          boxShadow: "none",
+          paddingLeft: {
+            md: "6rem",
+            xs: "3rem",
+          },
+          paddingRight: {
+            md: "6rem",
+            xs: "3rem",
+          },
+          height: "8em",
+          justifyContent: "center",
+        }}
+      >
+        <Toolbar>
+          <Link href="/" color="secondary" sx={{ textDecoration: "none", flexGrow: 1 }}>
+            <Typography variant="h4" color="secondary" fontWeight={600}>
+              Marlon Savian
+            </Typography>
+          </Link>
+          <IconButton aria-label="menu">
+            <MenuIcon
+              fontSize="large"
+              sx={{
+                width: "1.2em",
+                height: "1.2em",
+                color: { xs: "secondary.main", md: "primary.main" },
+              }}
+            />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 

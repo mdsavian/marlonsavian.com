015f992f551f8be75d969ba756eed41ecf74694a
mdsavian <marlon.savian@gmail.com>
2024-01-18 07:04:04 -0300

    feat(wip): adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        eletricBlue: "#4831d4",
+        lime: "#ccf381",
+      },
+    },
+  },
+  plugins: [],
+};

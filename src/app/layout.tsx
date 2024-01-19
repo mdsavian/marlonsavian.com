79cf7e022fdaa293e942740c62949e981a5cb6d2
mdsavian <marlon.savian@gmail.com>
2024-01-19 10:04:58 -0300

    feat(core): remove material ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "@/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,11 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

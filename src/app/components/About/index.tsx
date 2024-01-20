980e5474ff6fc4220679c916cb9b1cf28d605fe5
mdsavian <marlon.savian@gmail.com>
2024-01-20 10:48:02 -0300

    feat(about): fix header size and stroke options, add avatarShapes image'

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -1,18 +1,21 @@
 import AvatarShapes from "@/assets/avatar-shapes.svg";
+
 const About: React.FC = () => {
   return (
     <section className="sm:bg-about bg-blue pb-32 w-full relative">
-      <div className="flex flex-row items-center mt-48 max-w-screen-2xl py-0 px-[12vw]">
+      <div className="flex flex-row items-center mt-52 max-w-screen-2xl py-0 px-[12vw] justify-between">
         <div className="flex flex-col text-left relative">
           <h1 className="text-green text-7xl font-black max-w-[30vw]">Fullstack Developer</h1>
+
           <span className="text-white mt-8 text-2xl md:text-xl md:max-w-2xl sm:max-w-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            <br /> sed do eiusmod tempor incididunt <br />
+            ut labore et dolore magna aliqua.
           </span>
         </div>
 
-        <figure className="w-[23rem] h-[24rem] relative border-[0.115rem] border-solid border-white ">
-          {/* <AvatarShapes /> */}
+        <figure className="w-[23rem] h-[24rem] mr-20 relative border-[0.115rem] border-solid border-white ">
+          <AvatarShapes className="top-[-25%] w-[40rem] h-[126%] right-[-5.4rem] absolute" />
         </figure>
       </div>
       <div className="text-green max-w-screen-2xl py-0 px-[12vw] mt-12">
--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const Header = () => {
   return (
-    <nav className="bg-transparent px-12 md:px-20 h-32 z-50 flex items-center flex-wrap w-full fixed">
+    <nav className="bg-transparent px-12 md:px-20 h-32 z-50 flex items-center flex-wrap w-full h-44 fixed">
       <div className="flex grow items-center min-h-16">
         <div className="flex grow text-4xl font-bold ">
           <a href="/" className="block text-green">
@@ -18,8 +18,8 @@ const Header = () => {
             className="w-10 h-10 md:stroke-blue stroke-green"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
           </svg>

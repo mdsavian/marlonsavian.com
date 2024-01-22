7d37f7cab261f853ebd17cd22594308a5dce209f
mdsavian <marlon.savian@gmail.com>
2024-01-22 10:28:41 -0300

    refactor(about): extract photo to component

---
+++ src/app/components/About/Photo.tsx
@@ -0,0 +1,21 @@
+import PhotoShapes from "@/assets/photo-shapes.svg";
+
+const Photo = () => {
+  return (
+    <figure className="hidden sm:block w-[23rem] h-[24rem] mr-20 2xl:mr-[20rem] relative border-[0.115rem] border-solid border-white ">
+      <PhotoShapes className="top-[-25%] w-[40rem] h-[126%] right-[-5.4rem] absolute" />
+      <span
+        className="block mt-[-2.4rem] bg-white bg-cover ml-[-2.2rem]"
+        aria-label="marlon photo"
+        style={{
+          width: "inherit",
+          height: "inherit",
+          backgroundPositionX: "center",
+          backgroundImage: "url(marlon.jpeg)",
+        }}
+      />
+    </figure>
+  );
+};
+
+export default Photo;
--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -1,11 +1,11 @@
-import AvatarShapes from "@/assets/avatar-shapes.svg";
+import Photo from "./Photo";
 
 const About: React.FC = () => {
   return (
-    <section className="sm:bg-about bg-blue pb-32 w-full relative">
-      <div className="flex flex-row items-center mt-52 py-0 md:px-[12vw] sm:px-28 justify-between">
+    <section className="sm:bg-about bg-blue pb-12 md:pb-32 w-full relative">
+      <div className="flex flex-row items-center mt-52 py-0 md:px-[12vw] px-16 justify-between">
         <div className="flex flex-col text-left relative">
-          <h1 className="text-green text-7xl font-black max-w-[30vw]">Fullstack Developer</h1>
+          <h1 className="text-green text-7xl font-black max-w-[30vw] ">Fullstack Developer</h1>
 
           <span className="text-white mt-8 text-2xl md:text-xl md:max-w-2xl sm:max-w-xl">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
@@ -14,23 +14,11 @@ const About: React.FC = () => {
           </span>
         </div>
 
-        <figure className="w-[23rem] h-[24rem] mr-20 2xl:mr-[20rem] relative border-[0.115rem] border-solid border-white ">
-          <AvatarShapes className="top-[-25%] w-[40rem] h-[126%] right-[-5.4rem] absolute" />
-          <span
-            className="block mt-[-2.4rem] bg-white bg-cover ml-[-2.2rem]"
-            aria-label="marlon photo"
-            style={{
-              width: "inherit",
-              height: "inherit",
-              backgroundPositionX: "center",
-              backgroundImage: "url(marlon.jpeg)",
-            }}
-          />
-        </figure>
+        <Photo />
       </div>
-      <div className="text-green py-0 md:px-[12vw] sm:px-28 mt-12">
-        <ul className="flex md:text-lg sm:w-4/6 md:w-3/6 text-2xl xs:text-2xl ">
-          <li className="mr-3">
+      <div className="text-green py-0 md:px-[12vw] px-16 mt-32 md:mt-12">
+        <ul className="flex md:text-lg w-full sm:w-3/6 md:text-2xl text-lg">
+          <li className="md:mr-3 mr-6">
             Highly skilled at progressive enhancement, design systems &amp; UI Engineering.
           </li>
           <li>

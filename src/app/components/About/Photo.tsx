5df678bc9d3f037787c46d6b99a12adeb7dc1fc9
mdsavian <marlon.savian@gmail.com>
2024-01-23 15:47:10 -0300

    feat(about): center title and subtitle divs and fixed size

--- src/app/components/About/Photo.tsx
+++ src/app/components/About/Photo.tsx
@@ -6,7 +6,7 @@ interface IPhoto {
 }
 const Photo = ({ fillSvg, strokeSvg }: IPhoto) => {
   return (
-    <figure className="relative w-[20rem] sm:w-[23rem] h-[20rem] sm:h-[24rem] sm:mr-20 2xl:mr-[15rem] z-[1] ml-[2.2rem] sm:ml-0 border-[0.115rem] border-solid border-white">
+    <figure className="relative w-[20rem] sm:w-[23rem] h-[20rem] sm:h-[24rem] sm:mr-20 z-[1] ml-[2.2rem] sm:ml-0 border-[0.115rem] border-solid border-white">
       <PhotoShapes
         className="absolute top-[-25%] w-[35rem] sm:w-[40rem] h-[126%] right-[-3.4rem] sm:right-[-5.4rem] z-[-1]"
         fill={fillSvg}
--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -2,10 +2,12 @@ import Photo from "./Photo";
 
 const About: React.FC = () => {
   return (
-    <section className="sm:bg-about bg-blue pb-0 sm:pb-12 md:pb-32 w-full relative z-[-5]">
-      <div className="flex flex-row items-center mt-52 py-0 px-[12vw] px-16 justify-between">
+    <section className="flex flex-col sm:items-center justify-center sm:bg-about bg-blue pb-0 sm:pb-12 md:pb-32 w-full relative z-[-5]">
+      <div className="flex flex-row items-center mt-52 py-0 px-16 sm:px-40 justify-between w-full max-w-screen-2xl	2xl:max-w-[1700px]">
         <div className="flex flex-col text-left relative">
-          <h1 className="text-green text-7xl font-black max-w-[30vw] ">Fullstack Developer</h1>
+          <h1 className="text-green text-7xl font-black max-w-[30vw] ">
+            Software <br /> Engineer
+          </h1>
 
           <span className="text-white mt-8 text-2xl md:text-xl md:max-w-2xl sm:max-w-xl">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
@@ -18,7 +20,7 @@ const About: React.FC = () => {
           <Photo />
         </div>
       </div>
-      <div className="text-green py-0 px-[12vw] px-16 mt-32 md:mt-12 mb-16 sm:mb-0">
+      <div className="text-green py-0 px-16 sm:px-40 mt-32 md:mt-12 mb-16 sm:mb-0">
         <ul className="flex md:text-lg w-full sm:w-3/6 md:text-2xl text-lg">
           <li className="md:mr-3 mr-6">
             Highly skilled at progressive enhancement, design systems &amp; UI Engineering.

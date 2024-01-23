21e108766d97ed40dfa1d2f0293dcd3c743c889b
mdsavian <marlon.savian@gmail.com>
2024-01-23 15:49:22 -0300

    refactor(about): extract subtitle to component

---
+++ src/app/components/About/Subtitle.tsx
@@ -0,0 +1,11 @@
+const SubTitle = () => {
+  return (
+    <span className="text-white mt-8 text-2xl md:text-xl md:max-w-2xl sm:max-w-xl">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      <br /> sed do eiusmod tempor incididunt <br />
+      ut labore et dolore magna aliqua.
+    </span>
+  );
+};
+
+export default SubTitle;
--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -1,4 +1,5 @@
 import Photo from "./Photo";
+import SubTitle from "./Subtitle";
 import Title from "./Title";
 
 const About: React.FC = () => {
@@ -8,11 +9,7 @@ const About: React.FC = () => {
         <div className="flex flex-col text-left relative">
           <Title />
 
-          <span className="text-white mt-8 text-2xl md:text-xl md:max-w-2xl sm:max-w-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            <br /> sed do eiusmod tempor incididunt <br />
-            ut labore et dolore magna aliqua.
-          </span>
+          <SubTitle />
         </div>
 
         <div className="hidden sm:block">

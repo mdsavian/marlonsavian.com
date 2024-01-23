cfe951060c6228003e8b97f63fcb36370eb0b332
mdsavian <marlon.savian@gmail.com>
2024-01-23 09:24:40 -0300

    feat(about): hide photo when is big screen and show under first div

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -1,8 +1,9 @@
+import PhotoShapes from "@/assets/photo-shapes.svg";
 import Photo from "./Photo";
 
 const About: React.FC = () => {
   return (
-    <section className="sm:bg-about bg-blue pb-12 md:pb-32 w-full relative">
+    <section className="sm:bg-about bg-blue pb-0 sm:pb-12 md:pb-32 w-full relative z-[-5]">
       <div className="flex flex-row items-center mt-52 py-0 px-[12vw] px-16 justify-between">
         <div className="flex flex-col text-left relative">
           <h1 className="text-green text-7xl font-black max-w-[30vw] ">Fullstack Developer</h1>
@@ -14,9 +15,21 @@ const About: React.FC = () => {
           </span>
         </div>
 
-        <Photo />
+        <figure className="hidden sm:block w-[23rem] h-[24rem] mr-20 2xl:mr-[15rem] relative border-[0.115rem] border-solid border-white ">
+          <PhotoShapes className="top-[-25%] w-[40rem] h-[126%] right-[-5.4rem] absolute z-[-1]" />
+          <span
+            className="block mt-[-2.4rem] bg-white bg-cover ml-[-2.2rem]"
+            aria-label="marlon photo"
+            style={{
+              width: "inherit",
+              height: "inherit",
+              backgroundPositionX: "center",
+              backgroundImage: "url(marlon.jpeg)",
+            }}
+          />
+        </figure>
       </div>
-      <div className="text-green py-0 px-[12vw] px-16 mt-32 md:mt-12">
+      <div className="text-green py-0 px-[12vw] px-16 mt-32 md:mt-12 mb-16 sm:mb-0">
         <ul className="flex md:text-lg w-full sm:w-3/6 md:text-2xl text-lg">
           <li className="md:mr-3 mr-6">
             Highly skilled at progressive enhancement, design systems &amp; UI Engineering.
@@ -25,6 +38,25 @@ const About: React.FC = () => {
             Over a decade of experience building products for clients across several countries.
           </li>
         </ul>
+      </div>
+      <div className="block sm:hidden flex items-center justify-center pt-32 pb-20 bg-green">
+        <figure className="relative w-[20rem] h-[20rem] border-[0.115rem] border-solid border-white z-[1] ml-[2.2rem]">
+          <PhotoShapes
+            fill="blue"
+            stroke="blue"
+            className="top-[-25%] w-[35rem] h-[126%] right-[-3.4rem] absolute z-[-1]"
+          />
+          <span
+            className="block mt-[-2.4rem] bg-white bg-cover ml-[-2.2rem]"
+            aria-label="marlon photo"
+            style={{
+              width: "inherit",
+              height: "inherit",
+              backgroundPositionX: "center",
+              backgroundImage: "url(marlon.jpeg)",
+            }}
+          />
+        </figure>
       </div>
     </section>
   );

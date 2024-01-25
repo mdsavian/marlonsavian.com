949cd36ef3d74911ae856b8f6b22ded0a62237cd
mdsavian <marlon.savian@gmail.com>
2024-01-25 09:02:53 -0300

    feat(styles): add bluelight and purple color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       colors: {
         blue: "#4831d4",
         green: "#ccf381",
+        blueLight: "#F9F9F9",
+        purple: "#3d155f",
       },
       backgroundImage: (theme) => ({
         about: `linear-gradient(90deg, ${theme("colors.blue")} 67%, ${theme("colors.green")} 33%)`,

6306153c981d540580625f2da4353f1c430a5e0d
mdsavian <marlon.savian@gmail.com>
2024-01-25 09:03:27 -0300

    feat(about): create component and uses it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <main className={styles.main}>
         <Hero />
+        <About />
       </main>
     </div>
   );

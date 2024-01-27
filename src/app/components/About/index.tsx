1226ed28f4fbc86861242fe5e5435a68d8388114
mdsavian <marlon.savian@gmail.com>
2024-01-27 09:17:37 -0300

    feat(about): create about texts

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -1,49 +1,28 @@
-"use client";
-
-import PhotoShapes from "@/assets/photo-shapes.svg";
-import React, { useEffect, useState } from "react";
-
-const Container = ({ children }: { children: React.ReactNode }) => (
-  <section className="h-[50vh] overflow-hidden relative bg-blueLight p-12">{children}</section>
-);
-
-const AboutMeTitle = ({ scrollPercent }: { scrollPercent: number }) => (
-  <div
-    style={{ transform: `translateX(${scrollPercent}%)` }}
-    className="transition-transform duration-500 ease-out absolute left-[-15%]"
-  >
-    <p className="text-blue text-8xl font-black">ABOUT ME</p>
-    <p className="text-purple mt-16">
-      Crafting user-friendly and aesthetic UI designs is not just my profession, it's my passion.
-    </p>
-  </div>
-);
-
 const AboutMe = () => {
-  const [scrollPercent, setScrollPercent] = useState(0);
-
-  const handleScroll = () => {
-    const { body, documentElement } = document;
-    const sd = Math.max(body.scrollTop, documentElement.scrollTop);
-    const sp = (sd / (documentElement.scrollHeight - documentElement.clientHeight)) * 100;
-    const maxlimit = (documentElement.clientHeight * 150) / documentElement.scrollHeight;
-    if (sp >= 0 && sp <= maxlimit) {
-      setScrollPercent(sp);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <Container>
-      <PhotoShapes className="absolute w-[35rem] sm:w-[40rem] h-[126%] " />
-      <AboutMeTitle scrollPercent={scrollPercent} />
-    </Container>
+    <section className="flex flex-col sm:items-center justify-center bg-blueLight py-40 sm:pb-12 md:pb-32 w-full relative z-[-5]">
+      <div className="flex flex-col py-0 px-16 sm:px-40 justify-between w-full max-w-screen-2xl2xl:max-w-[1700px]">
+        <div className="flex flex-col text-left relative">
+          <h1 className="text-blue text-7xl font-black max-w-[30vw]">About me</h1>
+          <span className="text-purple mt-8 text-2xl md:text-xl md:max-w-2xl sm:max-w-xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </span>
+        </div>
+
+        <div className="mt-16 flex justify-end relative text-left">
+          <div className="flex flex-col">
+            <h1 className="text-blue text-7xl font-black max-w-[30vw]">Whats next</h1>
+            <span className="text-purple mt-8 text-2xl md:text-xl md:max-w-2xl sm:max-w-xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 

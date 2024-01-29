8ba4d289cbd808a9ae0fe63fff710b1270d95bc8
mdsavian <marlon.savian@gmail.com>
2024-01-29 18:40:28 -0300

    feat(about): adjust screen to be centered and not expand when is big screen

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -1,7 +1,7 @@
 const AboutMe = () => {
   return (
     <section className="flex flex-col sm:items-center justify-center bg-blueLight py-40 sm:pb-12 md:pb-32 w-full relative z-[-5]">
-      <div className="flex flex-col py-0 px-16 sm:px-40 justify-between w-full max-w-screen-2xl2xl:max-w-[1700px]">
+      <div className="flex flex-col py-0 px-16 sm:px-40 justify-between w-full max-w-screen-2xl 2xl:max-w-[1700px]">
         <div className="flex flex-col text-left relative">
           <h1 className="text-blue text-7xl font-black sm:max-w-[30vw] max-w-full">About me</h1>
           <span className="text-purple mt-8 text-2xl md:text-xl md:max-w-2xl sm:max-w-xl">
--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -6,7 +6,7 @@ import Title from "./Title";
 const Hero: React.FC = () => {
   return (
     <section className="flex flex-col sm:items-center justify-center sm:bg-about bg-blue pb-0 sm:pb-12 md:pb-32 w-full relative z-[-5]">
-      <div className="flex flex-row items-center mt-52 py-0 px-16 sm:px-40 justify-between w-full max-w-screen-2xl2xl:max-w-[1700px]">
+      <div className="flex flex-row items-center mt-52 py-0 px-16 sm:px-40 justify-between w-full max-w-screen-2xl 2xl:max-w-[1700px]">
         <div className="flex flex-col text-left relative">
           <Title />
           <SubTitle />
@@ -17,7 +17,7 @@ const Hero: React.FC = () => {
         </div>
       </div>
 
-      <div className="text-green py-0 px-16 sm:px-40 mt-32 md:mt-12 mb-16 sm:mb-0 w-full max-w-screen-2xl2xl:max-w-[1700px]">
+      <div className="text-green py-0 px-16 sm:px-40 mt-32 md:mt-12 mb-16 sm:mb-0 w-full max-w-screen-2xl 2xl:max-w-[1700px]">
         <Highlights />
       </div>
 

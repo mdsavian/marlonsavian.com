5bfe943dc3b285455dca685c84524fc791301826
mdsavian <marlon.savian@gmail.com>
2024-01-29 19:35:21 -0300

    feat(styles): add fade-in animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
       backgroundImage: (theme) => ({
         about: `linear-gradient(90deg, ${theme("colors.blue")} 67%, ${theme("colors.green")} 33%)`,
       }),
+      keyframes: {
+        fade: {
+          "0%": {
+            opacity: "0",
+            filter: "blur(4px)",
+          },
+          "100%": {
+            opacity: "1",
+            filter: "blur(0)",
+          },
+        },
+      },
+      animation: {
+        "span-1": "fade 0.8s 0.1s forwards cubic-bezier(0.11, 0, 0.5, 0)",
+        "span-2": "fade 0.8s 0.4s forwards cubic-bezier(0.11, 0, 0.5, 0)",
+        "span-3": "fade 0.8s 0.8s forwards cubic-bezier(0.11, 0, 0.5, 0)",
+      },
     },
   },
   plugins: [],

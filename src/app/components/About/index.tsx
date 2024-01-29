12eeab699f42eec64511cfad58708a1d28aaed85
mdsavian <marlon.savian@gmail.com>
2024-01-29 18:27:06 -0300

    feat(about): adjust title size on small screen

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -3,7 +3,7 @@ const AboutMe = () => {
     <section className="flex flex-col sm:items-center justify-center bg-blueLight py-40 sm:pb-12 md:pb-32 w-full relative z-[-5]">
       <div className="flex flex-col py-0 px-16 sm:px-40 justify-between w-full max-w-screen-2xl2xl:max-w-[1700px]">
         <div className="flex flex-col text-left relative">
-          <h1 className="text-blue text-7xl font-black max-w-[30vw]">About me</h1>
+          <h1 className="text-blue text-7xl font-black sm:max-w-[30vw] max-w-full">About me</h1>
           <span className="text-purple mt-8 text-2xl md:text-xl md:max-w-2xl sm:max-w-xl">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur
@@ -13,7 +13,7 @@ const AboutMe = () => {
 
         <div className="mt-16 flex justify-end relative text-left">
           <div className="flex flex-col">
-            <h1 className="text-blue text-7xl font-black max-w-[30vw]">Whats next</h1>
+            <h1 className="text-blue text-7xl font-black sm:max-w-[30vw] max-w-full">Whats next</h1>
             <span className="text-purple mt-8 text-2xl md:text-xl md:max-w-2xl sm:max-w-xl">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur

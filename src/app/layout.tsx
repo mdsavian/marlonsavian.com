13ec3d4a7a4b8dd34dd6cf2b77c898cf61acf425
mdsavian <marlon.savian@gmail.com>
2024-02-06 08:30:23 -0300

    feat(core): fix site title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Marlon Savian: Fullstack Developer ",
+  title: "Marlon Savian: Software Engineer",
   description: "Marlon Savian website",
 };
 

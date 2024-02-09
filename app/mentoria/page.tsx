a718166f3aaac6bdc369ac3e6592390b943b61e8
mdsavian <marlon.savian@gmail.com>
2024-02-09 08:52:04 -0300

    feat(mentorship): css adjusts for small screens

--- app/mentoria/page.tsx
+++ app/mentoria/page.tsx
@@ -3,13 +3,15 @@ const Mentoria: React.FC = () => {
     <div className="h-screen md:flex">
       <div className="h-screen relative overflow-hidden w-full flex bg-mentorship i justify-center items-center flex-col">
         <div className="px-8">
-          <h1 className="text-white font-bold text-4xl font-sans text-center">Mentoria</h1>
-          <p className="text-white mt-6">
-            Se cadastre para concorrer a uma hora de mentoria DE GRAÇA
+          <p className="text-white mt-6 text-center text-3xl font-sans">
+            Quer melhorar o seu perfil do LinkedIn <br />
+            com uma avaliação totalmente <strong>DE GRAÇA?</strong>
           </p>
         </div>
-        <form className="bg-white mt-24">
-          <h1 className="text-gray-800 font-bold text-2xl mb-7">Se cadastre!</h1>
+        <form className="mt-20 bg-white p-16 rounded-2xl">
+          <h1 className="text-gray-800 font-bold text-2xl text-center mb-7 font-sans">
+            Deixe seus dados abaixo!
+          </h1>
           <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -17,11 +19,7 @@ const Mentoria: React.FC = () => {
               viewBox="0 0 20 20"
               fill="currentColor"
             >
-              <path
-                fill-rule="evenodd"
-                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                clip-rule="evenodd"
-              />
+              <path d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" />
             </svg>
             <input
               className="pl-2 outline-none border-none w-full"
@@ -42,7 +40,7 @@ const Mentoria: React.FC = () => {
               <path d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
             </svg>
             <input
-              className="pl-2 outline-none border-none w-full"
+              className="pl-2 outline-none border-none w-full "
               type="text"
               name="email"
               id="email"
@@ -60,6 +58,7 @@ const Mentoria: React.FC = () => {
             >
               <path d="M3 5.5C3 14.0604 9.93959 21 18.5 21C18.8862 21 19.2691 20.9859 19.6483 20.9581C20.0834 20.9262 20.3009 20.9103 20.499 20.7963C20.663 20.7019 20.8185 20.5345 20.9007 20.364C21 20.1582 21 19.9181 21 19.438V16.6207C21 16.2169 21 16.015 20.9335 15.842C20.8749 15.6891 20.7795 15.553 20.6559 15.4456C20.516 15.324 20.3262 15.255 19.9468 15.117L16.74 13.9509C16.2985 13.7904 16.0777 13.7101 15.8683 13.7237C15.6836 13.7357 15.5059 13.7988 15.3549 13.9058C15.1837 14.0271 15.0629 14.2285 14.8212 14.6314L14 16C11.3501 14.7999 9.2019 12.6489 8 10L9.36863 9.17882C9.77145 8.93713 9.97286 8.81628 10.0942 8.64506C10.2012 8.49408 10.2643 8.31637 10.2763 8.1317C10.2899 7.92227 10.2096 7.70153 10.0491 7.26005L8.88299 4.05321C8.745 3.67376 8.67601 3.48403 8.55442 3.3441C8.44701 3.22049 8.31089 3.12515 8.15802 3.06645C7.98496 3 7.78308 3 7.37932 3H4.56201C4.08188 3 3.84181 3 3.63598 3.09925C3.4655 3.18146 3.29814 3.33701 3.2037 3.50103C3.08968 3.69907 3.07375 3.91662 3.04189 4.35173C3.01413 4.73086 3 5.11378 3 5.5Z"></path>
             </svg>
+
             <input
               className="pl-2 outline-none border-none w-full"
               type="text"

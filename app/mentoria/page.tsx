780ef5c89e1be7975c8e4ccec29061310fc4ca4e
mdsavian <marlon.savian@gmail.com>
2024-02-10 09:24:10 -0300

    feat(mentorship): fix form size

--- app/mentoria/page.tsx
+++ app/mentoria/page.tsx
@@ -5,10 +5,10 @@ const Mentoria: React.FC = () => {
         <div className="px-8">
           <p className="text-white mt-6 text-center text-3xl font-sans">
             Quer melhorar o seu perfil do LinkedIn <br />
-            com uma avaliação totalmente <strong>DE GRAÇA?</strong>
+            com uma avaliação totalmente <strong>GRÁTIS?</strong>
           </p>
         </div>
-        <form className="mt-20 bg-white p-16 rounded-2xl">
+        <form className="mt-20 bg-white p-8 md:p-16 rounded-2xl min-w-[320px]">
           <h1 className="text-gray-800 font-bold text-2xl text-center mb-7 font-sans">
             Deixe seus dados abaixo!
           </h1>
@@ -40,7 +40,7 @@ const Mentoria: React.FC = () => {
               <path d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
             </svg>
             <input
-              className="pl-2 outline-none border-none w-full "
+              className="pl-2 outline-none border-none w-full"
               type="text"
               name="email"
               id="email"

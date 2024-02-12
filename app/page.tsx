68eb4851093d7a6c35bb005ff1a66e63e22b3dab
mdsavian <marlon.savian@gmail.com>
2024-02-12 08:31:26 -0300

    feat(core): add the site on page again

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-// import Header from "./components/Header";
-// import Hero from "./components/Hero";
-// import About from "./components/About";
-import Mentoria from "./mentoria/page";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Bottom from "./components/Bottom";
 
 export default function Home() {
   return (
-    // <div className="relative">
-    //   <Header />
-    //   <main>
-    //     <Hero />
-    //     <About />
-    //   </main>
-    // </div>
-    <Mentoria />
+    <div className="relative">
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Bottom />
+      </main>
+    </div>
   );
 }

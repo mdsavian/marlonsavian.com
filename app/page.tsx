927d295c86fffef25ab6c5e2932c22320076e340
mdsavian <marlon.savian@gmail.com>
2024-02-13 08:32:04 -0300

    feat(footer): add footer component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Bottom from "./components/Bottom";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Hero />
         <About />
         <Bottom />
+        <Footer />
       </main>
     </div>
   );

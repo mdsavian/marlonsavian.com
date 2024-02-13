098c3d741ef656fbea44eca7fd3b1f1cc0688438
mdsavian <marlon.savian@gmail.com>
2024-02-13 08:32:28 -0300

    feat(bottom): fix text alignment

--- app/components/Bottom/index.tsx
+++ app/components/Bottom/index.tsx
@@ -1,9 +1,9 @@
 const Bottom = () => {
   return (
     <section className="flex flex-col items-center justify-center bg-blue py-32 sm:pb-12 md:pb-32 relative py-0 px-16 sm:px-40 relative">
-      <h1 className="text-green text-7xl font-black">Get In Touch</h1>
+      <h1 className="text-green text-7xl font-black text-center">Get In Touch</h1>
 
-      <span className="text-white mt-8 text-2xl md:text-xl">
+      <span className="text-white mt-8 text-2xl md:text-xl text-center">
         Although I’m not currently looking for any new opportunities, my inbox is always open.
         <br />
         Whether you have a question or just want to say hi, I’ll try my best to get back to you!
@@ -14,7 +14,7 @@ const Bottom = () => {
         href="mailto:marlon.savian@gmail.com"
         rel="noopener noreferrer"
         target="_blank"
-        className="font-bold text-center text-green hover:bg-green hover:text-white cursor-pointer bg-white py-2 px-8 rounded mt-16"
+        className="font-bold text-center text-green hover:bg-green hover:text-white cursor-pointer bg-white py-2 px-8 rounded mt-8"
       >
         Say Hello
       </a>

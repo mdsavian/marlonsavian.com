4f7ea9f234f379b73ee621a5d8e3cdf2506e45b2
mdsavian <marlon.savian@gmail.com>
2024-02-15 21:13:06 -0300

    feat(core): remove the site and shows only the mentorship form

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,18 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Bottom from "./components/Bottom";
+import Mentoria from "./mentoria/page";
 
 export default function Home() {
   return (
     <div className="relative">
-      <Header />
+      {/* <Header />
       <main>
         <Hero />
         <About />
         <Bottom />
-      </main>
+      </main> */}
+      <Mentoria />
     </div>
   );
 }
